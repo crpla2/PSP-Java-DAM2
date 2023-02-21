cf0b7a9ee060a05082b06fd3835cded9fb19733c
crpla2 <210176@iessierradeguara.com>
2023-02-21 02:12:34 +0100

    subo clientes de teo PSP

---
+++ clientes/src/main/java/cf/iesguara/clientes/model/S_Cliente.java
@@ -0,0 +1,28 @@
+package cf.iesguara.clientes.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name= "Sclientes")
+public class S_Cliente {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	private String nombre;
+	@Column(name= "primer_apellido", nullable = false)
+	private String primerApellido;
+	@Column(name= "segundo_apellido", nullable = true)
+	private String segundoApellido;
+	@Column(unique = true)
+	private String email;
+	
+}
